38a6f943e4376318acb3de310e2d1a41a44e609f
zy <javaweb_zy@163.com>
2019-09-19 21:58:22 +0800

    修改appserver

--- src/main/java/cn/edu/tjpu/AppServer.java
+++ src/main/java/cn/edu/tjpu/AppServer.java
@@ -1,8 +1,6 @@
 package cn.edu.tjpu;
 
-import cn.edu.tjpu.decoder.MyDecoder;
 import cn.edu.tjpu.serverhandler.PlanterServerHandler;
-import cn.edu.tjpu.utils.MyBatisUtil;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -11,58 +9,45 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
 
-/**
- * Hello world!
- */
 public class AppServer {
-    private int port = 8081;
     private static final Log LOGGER = LogFactory.getLog(AppServer.class);
 
-    public void start() {
+    public void start(int port) {
+        //配置nio服务端线程组
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
-        InputStream in = null;
         try {
             ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup,
-                    workerGroup).channel(NioServerSocketChannel.class).option(ChannelOption.SO_BACKLOG, 1024)
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            //	所有自定义的业务从这开始
-                            System.out.println("有客户端连接");
-                            ch.pipeline().addLast("decoder", new MyDecoder());
-                            ch.pipeline().addLast(new PlanterServerHandler());
-                        }
-                    });
-            ChannelFuture f = b.bind(this.port).sync();
-            MyBatisUtil.initDataSource();
-            System.out.println("服务端启动成功，端口为:" + port);
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024).childHandler(new ChildChannelHandler());
+            //绑定端口，同步等待成功
+            ChannelFuture f = b.bind(port).sync();
+            //等待服务端监听端口关闭
             f.channel().closeFuture().sync();
-        } catch (InterruptedException e) {
+        } catch (Exception e) {
             e.printStackTrace();
-            LOGGER.error("服务端启动失败", e);
         } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            workerGroup.shutdownGracefully();
+            //优雅退出，释放线程池资源
             bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
         }
+    }
 
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new FixedLengthFrameDecoder(23));
+            socketChannel.pipeline().addLast(new PlanterServerHandler());
+        }
     }
 
     public static void main(String[] args) {
-        new AppServer().start();
+        new AppServer().start(8080);
     }
 }
